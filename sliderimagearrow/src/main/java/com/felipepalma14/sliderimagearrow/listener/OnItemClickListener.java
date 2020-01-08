ca6279998e3fc49e4813e55450cb25edf79aa575
felipe-cunha <felipe.cunha@itriad.org.br>
2020-01-08 17:08:12 -0400

    Added Image Zoom

---
+++ sliderimagearrow/src/main/java/com/felipepalma14/sliderimagearrow/listener/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.felipepalma14.sliderimagearrow.listener;
+
+/**
+ * Created by Felipe Cunha on 08/01/20.
+ * Email: felipe.cunha@itriad.org.br
+ * Empresa: Instituto Triad
+ */
+public interface OnItemClickListener<T> {
+    void onItemClick(T item);
+}
